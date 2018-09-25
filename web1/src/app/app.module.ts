8319bd3b81297eb8da7ede1301daa940cf367187
GnitsIntern <gnitsintern@gmail.com>
2018-09-25 16:33:06 +0530

    third component

--- web1/src/app/app.module.ts
+++ web1/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ThirdComponent } from './third/third.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThirdComponent
   ],
   imports: [
     BrowserModule
